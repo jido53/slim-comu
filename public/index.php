c6364c5d9968465a0a79560fee96dbbd622a839d
Jido-U <gbuhl53@gmail.com>
2018-09-12 00:27:43 -0300

    probando twig

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<?php
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+require __DIR__ . '/../vendor/autoload.php';
+// Create app
+$app = new \Slim\App();
+
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('../templates', [
+        'cache' => '../cache'
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container->get('request')->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container->get('router'), $basePath));
+
+    return $view;
+};
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $this->view->render($response, 'profile.html', [
+        'name' => $args['name']
+    ]);
+})->setName('profile');
+
+// Run app
+$app->run();
+
+
+
+?>
